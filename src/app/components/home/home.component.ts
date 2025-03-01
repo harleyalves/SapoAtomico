0e6df5c9699e1e33d20455d8b1569de66836e827
harleyalves <75190014+harleyalves@users.noreply.github.com>
2025-03-01 18:59:00 -0300

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,24 +23,24 @@ export class HomeComponent implements AfterViewInit {
 
   constructor(@Inject(PLATFORM_ID) private platformId: object) {}
 
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      new Swiper('.swiper', {
-        modules: [Navigation, Pagination],
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        slidesPerView: 1,
-        spaceBetween: 30,
-      });
-    }
+ngAfterViewInit(): void {
+  if (isPlatformBrowser(this.platformId)) {
+    new Swiper('.swiper', {
+      modules: [Navigation, Pagination],
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,  
+      spaceBetween: 0,   
+    });
   }
+}
 
   openModal(imageUrl: string): void {
     this.selectedImage = imageUrl;
